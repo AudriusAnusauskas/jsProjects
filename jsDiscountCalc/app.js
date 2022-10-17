432bd2a8eeaa9412c529ed9aacffab72ecc0d7b7
AudriusAnusauskas <aaaudrius@gmail.com>
2022-10-17 21:23:44 +0300

    week

--- jsDiscountCalc/app.js
+++ jsDiscountCalc/app.js
@@ -1,5 +1,15 @@
-let check = "2022-09-05 Telia 50.00";
+let check = "2022-09-17 Telia 50.00";
 
 let date = new Date(Date.parse(check.slice(0, 10)));
 
 console.log(date);
+
+let dayOfWeek = date.getDay();
+
+console.log(dayOfWeek);
+
+if (dayOfWeek === 0 || dayOfWeek === 6) {
+  console.log("it is weekend");
+} else {
+  console.log("it is a workday");
+}
