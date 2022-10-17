7bc3db13b91334e44301a57a976aaeac33728dc8
AudriusAnusauskas <aaaudrius@gmail.com>
2022-10-17 23:44:23 +0300

    discount

--- jsDiscountCalc/app.js
+++ jsDiscountCalc/app.js
@@ -1,9 +1,17 @@
 let check = "2022-09-17 Telia 50.00";
 
+let merchantsDiscounts = {
+  Omni: "12%",
+  Telia: "20%",
+  CircleK: "15%",
+  Ruukki: "10%",
+};
+// Parse date
 let date = new Date(Date.parse(check.slice(0, 10)));
 
 console.log(date);
 
+//check if weekend
 let dayOfWeek = date.getDay();
 
 console.log(dayOfWeek);
@@ -13,3 +21,10 @@ if (dayOfWeek === 0 || dayOfWeek === 6) {
 } else {
   console.log("it is a workday");
 }
+
+//check mercant and discount for it
+Object.getOwnPropertyNames(merchantsDiscounts).forEach((val) => {
+  if (check.includes(val)) {
+    console.log(`Discount for ${val} is ${merchantsDiscounts[val]}`);
+  }
+});
