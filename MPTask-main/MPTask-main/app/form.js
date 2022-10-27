fef83520316b8bb3b5290d3e3d00714e907d1a70
AudriusAnusauskas <aaaudrius@gmail.com>
2022-10-27 21:51:40 +0300

    input

--- MPTask-main/MPTask-main/app/form.js
+++ MPTask-main/MPTask-main/app/form.js
@@ -1,9 +1,14 @@
 export function getTransaction() {
-    // use this function to get values from html input
-    return '2018-09-04 CIRCLE_K 50';
+  // use this function to get values from html input
+  let inputTotal = document.getElementById("transaction");
+  let transactionButton = document.getElementById("transactionButton");
+  transactionButton.addEventListener("click", function () {
+    inputTotal.value = `${date.value} ${merchants.value} ${amount.value}`;
+    return "2018-09-04 CIRCLE_K 50";
+  });
 }
 
 export function updateFee(transaction, fee) {
-    // use this function to get values from html input
-    console.log(fee);
+  // use this function to get values from html input
+  console.log(fee);
 }
