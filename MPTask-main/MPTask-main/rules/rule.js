35c03e80c8af0c4645d06ba37488b34ab8ffd3cc
AudriusAnusauskas <aaaudrius@gmail.com>
2022-10-31 21:00:27 +0200

    123

--- MPTask-main/MPTask-main/rules/rule.js
+++ MPTask-main/MPTask-main/rules/rule.js
@@ -1,18 +1,45 @@
+let merchantsDiscounts = {
+  Omni: ["12%", ["2022-09-17 Omni 1230"]],
+  Telia: ["10%", []],
+  Circle_K: ["20%", []],
+  Ruukki: ["10%", []],
+  "7-ELEVEN": ["22%", []],
+  Siemens: ["14%", []],
+};
+
 class Rule {
-    calculateDiscountPercent() {
-        return 0;
-    }
+  calculateDiscountPercent() {
+    Object.getOwnPropertyNames(e).forEach((val) => {
+      if (checkInput.includes(val)) {
+        merchant = val;
+        discount = e[val][0];
+        transactions = e[val][1];
+        transactions.push(checkInput);
+        transactionsNum = transactions.length;
+        document.querySelector(
+          "#p1"
+        ).innerText = `Discount for ${merchant} is ${discount}, 
+          ${merchant} has made ${transactionsNum} transactions`;
+        console.log(transactions, transactionsNum);
+      } else if (merchant === undefined) {
+        document.querySelector("#p1").innerText = `no such merchant`;
+        document.querySelector("#p2").innerText = ` `;
+      }
+    });
+    return [discount, merchant, transactions, transactionsNum];
+    return 0;
+  }
 
-    calculateDiscountFlatAmount() {
-        return 0;
-    }
+  calculateDiscountFlatAmount() {
+    return 0;
+  }
 
-    apply() {
-        return {
-            discountPercent: this.calculateDiscountPercent(),
-            discountFlatAmount: this.calculateDiscountFlatAmount()
-        }
-    }
+  apply() {
+    return {
+      discountPercent: this.calculateDiscountPercent(),
+      discountFlatAmount: this.calculateDiscountFlatAmount(),
+    };
+  }
 }
 
 export default Rule;
